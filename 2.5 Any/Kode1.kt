8f34b1429c0212cae28b939ea898b057af572d5b
Naufal Ma'ruf Ashrori <128896038+naufal225@users.noreply.github.com>
2024-05-17 13:22:23 +0700

    Create Kode1.kt

---
+++ 2.5 Any/Kode1.kt
@@ -0,0 +1,6 @@
+class Laptop(val brand: String) // sama saja dengan class Laptop(): Any
+class Komputer(val brand: String) // sama saja dengan class Laptop(): Any()
+
+fun main() {
+    val laptopmsi = Laptop("MSI")
+}
